9af0231ad48374d0953e4cdf9ce740aafd59d015
QuitR <striupasa96@gmail.com>
2016-12-12 16:11:24 +0200

    Program base
    EF from model, database

---
+++ PartShop/PartShop/Model1.edmx.sql
@@ -0,0 +1,132 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 12/12/2016 15:42:29
+-- Generated from EDMX file: C:\Users\Andrius\Desktop\C#\PartShop\PartShop\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Database];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_UserItem]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[ItemSet] DROP CONSTRAINT [FK_UserItem];
+GO
+IF OBJECT_ID(N'[dbo].[FK_ItemCategory]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[ItemSet] DROP CONSTRAINT [FK_ItemCategory];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[UserSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[UserSet];
+GO
+IF OBJECT_ID(N'[dbo].[CategorySet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[CategorySet];
+GO
+IF OBJECT_ID(N'[dbo].[ItemSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[ItemSet];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'UserSet'
+CREATE TABLE [dbo].[UserSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Username] nvarchar(max)  NOT NULL,
+    [Password] nvarchar(max)  NOT NULL,
+    [IsAdmin] bit  NOT NULL
+);
+GO
+
+-- Creating table 'CategorySet'
+CREATE TABLE [dbo].[CategorySet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'ItemSet'
+CREATE TABLE [dbo].[ItemSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Price] float  NOT NULL,
+    [Make] nvarchar(max)  NOT NULL,
+    [Model] nvarchar(max)  NOT NULL,
+    [ReleaseDate] datetime  NULL,
+    [CountInStorage] int  NULL,
+    [UserId] int  NOT NULL,
+    [CategoryId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'UserSet'
+ALTER TABLE [dbo].[UserSet]
+ADD CONSTRAINT [PK_UserSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'CategorySet'
+ALTER TABLE [dbo].[CategorySet]
+ADD CONSTRAINT [PK_CategorySet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'ItemSet'
+ALTER TABLE [dbo].[ItemSet]
+ADD CONSTRAINT [PK_ItemSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [UserId] in table 'ItemSet'
+ALTER TABLE [dbo].[ItemSet]
+ADD CONSTRAINT [FK_UserItem]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[UserSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserItem'
+CREATE INDEX [IX_FK_UserItem]
+ON [dbo].[ItemSet]
+    ([UserId]);
+GO
+
+-- Creating foreign key on [CategoryId] in table 'ItemSet'
+ALTER TABLE [dbo].[ItemSet]
+ADD CONSTRAINT [FK_ItemCategory]
+    FOREIGN KEY ([CategoryId])
+    REFERENCES [dbo].[CategorySet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ItemCategory'
+CREATE INDEX [IX_FK_ItemCategory]
+ON [dbo].[ItemSet]
+    ([CategoryId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
